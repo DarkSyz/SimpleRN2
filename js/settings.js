16fbd0c8bd750f0c8b0c3c7cba3fe19e49665bd7
DarkSyz <darksys@sina.com>
2017-08-06 22:29:45 +0800

    reset navigation stack when logout

--- js/settings.js
+++ js/settings.js
@@ -6,13 +6,21 @@ import {
     TextInput,
     Image
 } from 'react-native';
+import { NavigationActions } from 'react-navigation';
 
 export default SettingsScreen = (props) =>
     <View>
-        <Button color={'green'} title='Logout' onPress={() =>
-            props.navigation.navigate('Login', {})
+        <Button color={'green'} title='Logout' onPress={
+            () => {
+                props.navigation.dispatch(NavigationActions.reset({
+                    index: 0,
+                    actions: [
+                        NavigationActions.navigate({ routeName: 'Login' })
+                    ]
+                }));
+            }
         } />
         <Button color={'red'} title='RaiseError' onPress={() =>
-            throws ('Error!')
+            throws('Error!')
         } />
     </View>;
