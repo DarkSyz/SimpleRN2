14d7bc148270ec43868af2fcaf702c6311a7ceae
DarkSyz <darksys@sina.com>
2017-08-15 23:21:55 +0800

    global handler

--- index.ios.js
+++ index.ios.js
@@ -5,8 +5,28 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Alert } from 'react-native';
 import App from './js/app';
+
+import ErrorUtils from "ErrorUtils";
+(async function initUncaughtErrorHandler() {
+    const defaultGlobalHandler = ErrorUtils.getGlobalHandler();
+ 
+    ErrorUtils.setGlobalHandler(async(error, isFatal) => {
+        try {
+            if (!__DEV__) {
+                Alert.alert(error.message);
+            }
+        }
+        catch (error) {
+            Alert.alert(error.message);
+        }
+ 
+        if (__DEV__ && defaultGlobalHandler) {
+            defaultGlobalHandler(error, isFatal);
+        }
+    });
+})();
 
 export default class SimpleRN2 extends Component {
   render() {
